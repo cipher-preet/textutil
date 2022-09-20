6fea3896c83570c640f2b60b07647f6be4111593
cipher-preet <112680439+cipher-preet@users.noreply.github.com>
2022-09-20 13:43:37 +0530

    Updates

---
+++ precache-manifest.d31ce85cf664aeb5cb072c0cc27047c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cf9c59de7c9f0a5fbba",
+    "url": "/textutil/static/css/main.22f38cdb.chunk.css"
+  },
+  {
+    "revision": "5cf9c59de7c9f0a5fbba",
+    "url": "/textutil/static/js/main.5cf9c59d.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/textutil/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutil/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "b01e01129c4457dfdea6",
+    "url": "/textutil/static/js/runtime~main.b01e0112.js"
+  },
+  {
+    "revision": "58b749d07623dffcb4137e56bb915145",
+    "url": "/textutil/index.html"
+  }
+];
